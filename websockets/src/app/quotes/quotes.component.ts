c33c8fe93b1751aa6a86743153d774285a21d8fc
AndrewBellamy <andymanbellamy@gmail.com>
2018-05-22 15:03:46 +0930

    Websockets completed

--- websockets/src/app/quotes/quotes.component.ts
+++ websockets/src/app/quotes/quotes.component.ts
@@ -1,26 +1,11 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { WebsocketService } from './websocket.service';
 
 @Component({
   selector: 'quotes',
-  template: `<div>
-  WebSocket Status: <span [ngClass]='{closed: status===0, open:status===1}'>
-  {{statusText[status]}}</span><br>
-  <button (click)='startWebsocketCommunications()'>Start</button>
-  <button (click)='stopWebsocketCommunications()'>Stop</button>
-  <button (click)='resetMessages()'>Reset</button>
-  <p *ngFor="let quote of quotes" >
-    {{quote.ticker}},{{quote.price}},{{quote.change}}
-  </p>
-  </div>
-  `,
-  styles: [`
-  div{background-color:antiquewhite;display: inline-block; padding:5px;}
-  button{margin:5px;}
-  span{font-weight:bold;}
-  p{color:darkmagenta;margin:0px;background-color:#ddd; text-align:center;}
-  .closed{color:crimson;}
-  .open{color:#0c0;}
-  `]
+  templateUrl: './quotes.component.html',
+  styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent  {
   quotes: Array<{}> = [];
@@ -28,21 +13,30 @@ export class QuotesComponent  {
   statusText: string[] = ['Closed', 'Open'];
   url: string = 'ws://localhost:4000';
 
-  constructor(){}
+  constructor(private webSocketService: WebsocketService){}
 
   startWebsocketCommunications(){
-    // add code here
-    alert('not implemented yet');
+    let obs: Observable<{}> = this.webSocketService.getMessages()
+
+    this.status = 1 //Means open
+    obs.subscribe((msg: string) => {
+      console.log('msg: ' + msg)
+      this.quotes.push(JSON.parse(msg))
+    })
   }
 
   stopWebsocketCommunications(){
-    // add code here
-    alert('not implemented yet');
+    this.webSocketService.stopMessages()
+    this.status = 0 //Means closed
   }
 
   resetMessages(){
-    // add code here
-    alert('not implemented yet');
+    if (this.status === 1) {
+      this.stopWebsocketCommunications()
+      this.quotes = []
+    } else {
+      this.quotes = []
+    }
   }
 
 }
