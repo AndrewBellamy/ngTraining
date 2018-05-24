61c67421e0ee2b592c86fbfcc0efbaf3a712bc26
AndrewBellamy <andymanbellamy@gmail.com>
2018-05-24 14:57:41 +0930

    added a little extra to animations

--- angular-animations/src/app/about/about.component.ts
+++ angular-animations/src/app/about/about.component.ts
@@ -5,16 +5,22 @@ import { Animations } from '../animations';
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css'],
-  animations: [ Animations.page ]
+  animations: [ Animations.page, Animations.shadow ]
 })
 
 export class AboutComponent implements OnInit {
+
+  public state = 'off'
 
   @HostBinding('@routeAnimation') anyProperty = 'anything'
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleState() {
+    this.state = this.state === 'off' ? 'on' : 'off'
   }
 
 }
--- angular-animations/src/app/animations.ts
+++ angular-animations/src/app/animations.ts
@@ -5,9 +5,19 @@ export class Animations {
     static styleCurrent = style({transform: 'translateX(0)', opacity: 1});
     static styleEnd = style({transform: 'translateX(100%)', opacity: 0});
 
+    static styleShadowed = style({boxShadow: '5px 10px 18px #888888'})
+    static styleUnshadowed = style({boxShadow: 'none'})
+
     static page = trigger('routeAnimation', [
         state('*', Animations.styleCurrent),
         transition('void => *', [Animations.styleStart, animate('0.5s ease-in-out')]),
         transition('* => void', animate('0.5s ease-in-out', Animations.styleEnd))
     ]);
+
+    static shadow = trigger('hoverAnimation', [
+        state('off', Animations.styleUnshadowed),
+        state('on', Animations.styleShadowed),
+        transition('off => on', animate('0.2s ease-in')),
+        transition('on => off', animate('0.2s ease-out'))
+    ])
 }
